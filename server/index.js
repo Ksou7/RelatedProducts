c3cb9b3c1433a9e1ac13c2ff430c75a0a147ee0f
MalekWahmi <wahmimalek@gmail.com>
2021-03-02 14:37:32 +0100

    ratingbackendfinish

--- server/index.js
+++ server/index.js
@@ -5,16 +5,68 @@ const app = express();
 const port = process.env.PORT || 3001;
 const e = require("express");
 const axios = require("axios");
-const env = require("dotenv").config()
+const env = require("dotenv").config();
 const { send } = require("process");
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "../public")));
 
+//this get request for the product
+// app.get("/api/products/:product_id", async (req, res) => {
+//   var data = [];
+//   await axios
+//     .get(
+//       `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${req.params.product_id}/related`,
+//       {
+//         headers: {
+//           Authorization: process.env.TOKEN,
+//         },
+//         _id: req.params.product_id,
+//       }
+//     )
+//     .then(async (related) => {
+//       //loop to get the related
+//       for (var i = 0; i < related.data.length; i++) {
+//         await axios
+//           .get(
+//             `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${related.data[i]}`,
+//             {
+//               headers: {
+//                 Authorization: process.env.TOKEN,
+//               },
+//             }
+//           )
+//           .then((product) => data.push(product.data))
+//           .catch((err) => console.log(err));
+//         // this get request to get the style from the related data
+//         await axios
+//           .get(
+//             `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${related.data[i]}/styles`,
+//             {
+//               headers: {
+//                 Authorization: process.env.TOKEN,
+//               },
+//             }
+//           )
+//           .then((style) => {
+//             // console.log(style);
+//             // console.log(style.data.results[0].photos[0]);
+//             if (style.data.results[0].photos) {
+//               data[i].url = style.data.results[0].photos[0];
+//             }
+//           });
+//       }
+//     })
+//     .catch((err) => console.log(err));
 
-app.get("/api/products/:product_id", async (req, res) => {
-  var data = [];
-  // console.log(req.params.prostyleduct_id);
+//   console.log(data);
+//   res.send(data);
+// });
+
+//this Get request to fetch  Rating data
+
+app.get("/reviews", async (req, res) => {
+  var related = [];
   await axios
     .get(
       `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${req.params.product_id}/related`,
@@ -25,69 +77,44 @@ app.get("/api/products/:product_id", async (req, res) => {
         _id: req.params.product_id,
       }
     )
-    .then(async (related) => {
-      // console.log(related.data);
-
-      for (var i = 0; i < related.data.length; i++) {
+    .then(async (product) => {
+      for (var j = 0; j < product.lenght; j++) {
         await axios
           .get(
-            `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${related.data[i]}`,
+            `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${product[i]}`,
             {
               headers: {
                 Authorization: process.env.TOKEN,
               },
             }
           )
-          .then((product) => data.push(product.data))
-          .catch((err) => console.log(err));
+          .then((result) => {
+            related.push(result);
+            console.log(result);
+          });
         await axios
           .get(
-            `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${related.data[i]}/styles`,
+            `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/products/${product[i]}`,
             {
               headers: {
                 Authorization: process.env.TOKEN,
               },
             }
           )
-          .then((style) => {
-            // console.log(style);
-            // console.log(style.data.results[0].photos[0]);
-            if (style.data.results[0].photos) {
-              data[i].url = style.data.results[0].photos[0];
+          .then((rating) => {
+            console.log(rating);
+            if (related.rating) {
+              rating.rating = star.rating;
             }
           });
       }
     })
-    .catch((err) => console.log(err));
-
-  console.log(data);
-  res.send(data);
+    .catch((err) => {
+      console.log(err);
+    });
+  res.send(related);
 });
-
-
-// Get Rating
-
-
-app.get("/reviews", async (req, res) => {
-  var rating =[]
-  await axios
-    .get(
-      `https://app-hrsei-api.herokuapp.com/api/fec2/hrnyc/reviews/${req.params.product_id}`,
-      {
-        headers: {
-          Authorization: process.env.TOKEN,
-        },
-        _id: req.params.product_id,
-      })
-   .then((product)=>{
-    res.send(product)
-    .catch((err) => ((console.log(err))
-   })
-  }
-
-
-
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
-})
+});
